e3e6f927c208099872e218d04cae12eb0de4ec73
rgfigueroa <roberthgfd@gmail.com>
2020-11-11 00:51:04 -0500

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,20 @@
+public class Demo{ 
+	public static void main(String[] arg){
+		int x=2;
+		int $contador=0;
+		//int 1var=0;
+		int a=0;
+		int b=1;
+		int v1=20;
+		x = a+b;
+		/*
+		if (x < 5 & x != 4) { 
+			System.out.println("Hola Mundo");
+		} */
+		System.out.println("Valor:"+v1);
+		if (x == 2) {
+			System.out.println("Es true");
+		}
+	}
+
+}
